dfeb0ceace59713808f8b841a030ac3ab2422731
kalitonleite-create <kalitonleite@gmail.com>
2026-03-20 14:47:04 -0400

    feat: initialize and export Neon database connection utility.

--- api/db.js
+++ api/db.js
@@ -4,12 +4,9 @@ if (!process.env.NEON_DATABASE_URL) {
   throw new Error('NEON_DATABASE_URL nao definida.');
 }
 
-const sql_conn = neon(process.env.NEON_DATABASE_URL);
-
-// Standard tagged template literal
-const sql = (strings, ...values) => sql_conn(strings, ...values);
-
-// Add .query method for dynamic queries (needed by data.js)
-sql.query = (query, params) => sql_conn(query, params);
+// neon() returns an object that:
+// 1. Can be used as a tagged template literal (sql`...`)
+// 2. Has a .query(queryString, params) method for conventional queries
+const sql = neon(process.env.NEON_DATABASE_URL);
 
 module.exports = { sql };
