a97076671c9b6438e9ea4fb6eba40957f6b734a4
kalitonleite <kalitonleite@gmail.com>
2026-03-23 11:53:58 -0400

    style: fix logo image scaling in admin register page and finalize all auth pages

--- pages/AdminRegisterPage.tsx
+++ pages/AdminRegisterPage.tsx
@@ -73,7 +73,7 @@ const AdminRegisterPage: React.FC<AdminRegisterPageProps> = ({ onLogin }) => {
                 <i className="fas fa-arrow-left"></i>
               </button>
               <div className="bg-white rounded-2xl shadow-xl border border-slate-50 overflow-hidden w-20 h-20">
-                <img src={appLogo || "/assets/logo-uarini.jpg"} alt="Logo" className="w-12 h-12 object-contain" />
+                <img src={appLogo || "/assets/logo-uarini.jpg"} alt="Logo" className="w-full h-full object-cover" />
               </div>
             </div>
 
