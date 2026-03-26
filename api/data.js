7a45e8688e2b524e25f950130c21be077be33ea6
kalitonleite <kalitonleite@gmail.com>
2026-03-25 20:42:05 -0400

    security: allow public select access to qr_codes and specific appointment IDs for the scanner flux

--- api/data.js
+++ api/data.js
@@ -20,7 +20,9 @@ module.exports = async function handler(req, res) {
     const { table, action, id, filter, order, data } = body;
 
     const isPublicCheck = (table === 'authorization_codes' && action === 'select') ||
-                          (table === 'lab_settings' && action === 'select');
+                          (table === 'lab_settings' && action === 'select') ||
+                          (table === 'qr_codes' && action === 'select') ||
+                          (table === 'appointments' && action === 'select' && filter && filter.id);
 
     if (!isPublicCheck) {
       const authHeader = req.headers.authorization;
