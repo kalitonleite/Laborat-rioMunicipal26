fa988560e4f45706212766dc7ecf32de8bd0696a
kalitonleite <kalitonleite@gmail.com>
2026-04-06 09:03:07 -0400

    Remove bodyParser config that was crashing Vercel Serverless edge functions with a 500 error

--- api/data.js
+++ api/data.js
@@ -117,11 +117,3 @@ module.exports = async function handler(req, res) {
     return res.status(500).json({ error: 'Erro no banco de dados: ' + err.message });
   }
 };
-
-module.exports.config = {
-  api: {
-    bodyParser: {
-      sizeLimit: '10mb',
-    },
-  },
-};
